5a88e7b84aff1b82f914c01e980133574b065884
Jinu Lee <aquamrn@snu.ac.kr>
2021-03-12 23:40:31 +0000

    Change directory tree

---
+++ embedding/bert_embeddings.py
@@ -0,0 +1,79 @@
+"""
+@module bert_embedding
+Various transformer-based embedding modules.
+
+Class name format: 
+  Embedding(description)_language
+Requirements:
+  __init__(self, config)
+    @param config Dictionary. Configuration for Embedding*_* (free-format)
+  self.embed_size
+  forward(self, inputs)
+    @param inputs List of dictionaries. Refer to 'corpus.py' for more details.
+    @return output Tensor(batch, length+1, self.embed_size). length+1 refers to the virtual root.
+"""
+
+import torch
+import torch.nn as nn
+from transformers import BertModel
+from kobert_transformers import get_tokenizer
+
+class EmbeddingBERT_kor(nn.Module):
+  """
+  Embedding model using KoBERT(SKT-brain, 2019).
+  Concatenates the hidden state of initial & final wordpiece tokens.
+  """
+  def __init__(self, config):
+    """
+    Constructor for EmbeddingBERT_kor.
+    
+    @param self The object pointer.
+    @param config Dictionary. Configuration for EmbeddingBERT_kor(unused)
+    """
+    super(EmbeddingBERT_kor, self).__init__()
+    self.tokenizer = get_tokenizer()
+    self.model = BertModel.from_pretrained('monologg/kobert')
+    self.model.eval()
+    self.model.requires_grad = False
+    self.embed_size = 1536
+
+  def forward(self, inputs):
+    """
+    Overridden forward().
+    
+    @param self The object pointer.
+    @param inputs List of dictionary. Refer to 'corpus.py' for more details.
+    
+    @return Tensor(batch, length, self.embed_size)
+    """
+    device = next(self.parameters()).device
+    
+    tokens_list = []
+    sentences = []
+    attention_mask = []
+    for input in inputs:
+      tokens = self.tokenizer.tokenize('[CLS] '+input['text']+' [SEP]')
+      tokens_list.append(tokens)
+      tokens = self.tokenizer.convert_tokens_to_ids(tokens)
+      sentences.append(torch.tensor(tokens, dtype=torch.long))
+      attention_mask.append(torch.ones([len(tokens)], dtype=torch.long))
+    sentences = nn.utils.rnn.pad_sequence(sentences, batch_first=True).to(device)
+    attention_mask = nn.utils.rnn.pad_sequence(attention_mask, batch_first=True).to(device)
+
+    output = self.model(sentences, attention_mask, torch.zeros_like(attention_mask))['last_hidden_state']
+    embedded = []
+    temp = None
+    for i, tokens in enumerate(tokens_list):
+      embedded.append([])
+      for j in range(len(tokens)):
+        if tokens[j] == '[SEP]':
+          break
+        if tokens[j] == '[CLS]' or tokens[j].startswith('▁'):
+          temp = output[i][j]
+        if j+1 == len(tokens) or tokens[j+1] == '[SEP]' or tokens[j+1].startswith('▁'):
+          temp = torch.cat([temp, output[i][j]], 0)
+          embedded[i].append(temp.unsqueeze(0))
+          temp = None
+      embedded[i] = torch.cat(embedded[i], 0)
+    embedded = nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+    return embedded
---
+++ embedding/dict_embedding.py
@@ -0,0 +1,142 @@
+"""
+@module dict_embedding
+Various lexicon-based embedding modules.
+
+Class name format: 
+  Embedding(description)_language
+Requirements:
+  __init__(self, config)
+    @param config Dictionary. Configuration for Embedding*_* (free-format)
+  self.embed_size
+  forward(self, inputs)
+    @param inputs List of dictionaries. Refer to 'corpus.py' for more details.
+    @return output Tensor(batch, length+1, self.embed_size). length+1 refers to the virtual root.
+"""
+
+import torch
+import torch.nn as nn
+import json
+
+class EmbeddingDict(nn.Module):
+  """
+  Abstract Embedding model that creates embedding based on lexicon data.
+  """
+  
+  def __init__(self, config):
+    """
+    Constructor for EmbeddingDict.
+    All embedding files should match the following format:
+     [
+        {
+          "text": "language",
+          "pos": "language/NNG", (optional, any tags)
+          "embedding": "1.0 2.1 -0.9 ...", (mandatory if config['from_pretrained']')
+          ...
+        },
+        ...
+     ]
+     UNK tokens are mapped to the last of the sequence(i.e. -1 th index).
+    
+    @param self The object pointer.
+    @param config Dictionary. Configuration for the embedding.
+    """
+    super(EmbeddingDict, self).__init__()
+    
+    self.file_dir = config['file_directory']
+    with open(self.file_dir) as embedding_file:
+      self.full_information = json.loads(embedding_file)
+     
+    if 'key' in config:
+      key = config['key']
+    else:
+      key = 'text'
+        
+    self.itos = []
+    self.stoi = {}
+    self.embeddings = []
+    for i, info in enumerate(self.full_information):
+      self.itos.append(info[key])
+      self.stoi[info[key]] = i
+      if config['from_pretrained']:
+        self.embeddings.append(torch.FloatTensor([float(x) for x in split(info['embedding'])]).unsqueeze(0))
+    
+    if config['from_pretrained']:
+      # Read pretrained embedding
+      self.embed_size = self.embeddings[0].size(0)
+      self.embeddings.append(torch.zeros(self.embed_size).unsqueeze(0))
+      self.embeddings = nn.Embedding.from_pretrained(torch.cat(self.embeddings, 0))
+    else:
+      # From-Scratch embedding
+      self.embed_size = config['embed_size']
+      self.embeddings = nn.Embedding(len(self.itos) + 1, config['embed_size'])
+    
+    self.requires_grad = bool(config['train'])
+    self.stoi['UNK'] = len(self.itos)
+    self.itos.append('UNK')
+
+
+class EmbeddingDictSelectPoS_kor(EmbeddingDict):
+  """
+  Abstract template for Korean lexicon-style embeddings. Implements practical PoS tag selection.
+  
+  Korean "pos" format:
+        [
+          ["", ""],
+          ["", "", ""],
+          ...
+        ]
+  
+  Follows Mecab morpheme tagging indications.
+  """
+  def __init__(self, config):
+    super(EmbeddingDictSelectPoS_kor, self).__init__(config)
+  
+  def forward(self, inputs):
+    lexical_morphemes_tag = [
+      'NNG', 'NNP', 'NNB', 'NNBC', 'NR', 'NP',
+      'VV', 'VA', 'VX', 'VCP', 'VCN',
+      'MM' 'MAG', 'MAJ',
+      'IC', 'SL'
+    ]
+    embedded = []
+    for i, input in enumerate(inputs):
+      assert 'pos' in input
+      
+      embedded.append([])
+      
+      for word in input[pos]:
+        word_embedding = torch.zeros(2 * self.embed_size)
+        for morph in word:
+          pos_tag = morph.split('/', -1)
+          if pos_tag in lexical_morphemes_tag:
+            word_embedding[:self.embed_size] = self.embedding[self.stoi[self.target(morph)]]
+          else:
+            word_embedding[self.embed_size:] = self.embedding[self.stoi[self.target(morph)]]
+            
+        embedded[i].append(torch.Tensor(word_embedding))
+    
+    embedded = torch.nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+      
+    return embedded
+
+
+class EmbeddingGloVe_kor(EmbeddingDictSelectPoS_kor):
+  def __init__(self, config):
+    super(EmbeddingGloVe_kor).__init__(config)
+  
+  def target(word):
+    if word in self.stoi:
+      return word
+    else:
+      return 'UNK'
+
+class EmbeddingPoS_kor(EmbeddingDictSelectPoS_kor):
+  def __init__(self, config):
+    super(EmbeddingGloVe_kor).__init__(config)
+  
+  def target(word):
+    target = word.split('/', -1)
+    if target in self.stoi:
+      return target
+    else:
+      return 'UNK'
