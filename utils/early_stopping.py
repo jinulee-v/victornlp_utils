6ed35b41e519d43ee37618e362bced830727f1d0
Jinu <aquamrn@snu.ac.kr>
2021-03-27 08:35:40 +0900

    implemented EarlyStopping module

---
+++ utils/early_stopping.py
@@ -0,0 +1,100 @@
+"""
+@module Implements early stopping.
+"""
+
+import logging
+
+class EarlyStopping():
+  """
+  @brief Criterion-based early stopping.
+
+  Implments basic early stopping and model saving.
+  Can save the best checkpoint by the valiadation loss.
+
+  Usage:
+  early_stopper = EarlyStopping() # initialize
+  ...
+  if early_stopper(epoch, loss, model, '.../.../.../model.pt'): # tries to early stop
+    # early stopping the training sequence
+    exit()
+  # continue training
+
+  Notes:
+  This class makes use of the logging library for logging states.
+  """
+
+  def __init__(self, patience=3, eps=0.0, maximize=False):
+    """
+    @brief initializer for the EarlyStopping class.
+
+    Waits for patience epochs even the criterion is not improved by eps.
+
+    @param patience Patience.
+    @param eps Epsilon.
+    @param maximize If True, tries to maximize the criterion instead of minimizing it. Default: False.
+    """
+    self.patience = patience
+    self.eps = eps
+    self.maximize = maximize
+
+    assert eps > 0
+
+    self.best_epoch = 0
+    if maximize:
+      self.best = float('-inf')
+    else:
+      self.best = float('inf')
+    self.waits = 0
+    self.logger = logging.getLogger()
+  
+  def __call__(self, epoch, criterion, model, save_dir):
+    """
+    @brief __call__ impl for early stopping.
+
+    @param epoch Current epoch number for logging.
+    @param criterion Value to update.
+    @param model Model to save.
+    @param save_dir Directory to save model.
+
+    @return True if patience ran out (i.e. needs to early stop). False otherwise.
+    """
+    self.logger.info('Early stopping...')
+
+    if maximize:
+      if criterion > self.best + self.eps:
+        self.logger.info('Updating best epoch...')
+        self.logger.info('- Current epoch: {}', criterion)
+        self.logger/info('- Previous best epoch: {} (epoch: {})', self.best, self.best_epoch)
+        self.best_epoch = epoch
+        self.best = criterion
+
+        torch.save(model.state_dicts(), save_dir)
+      else:
+        self.logger.info('Not updating...')
+        self.logger.info('- Current epoch: {}', criterion)
+        self.logger/info('- Previous best epoch: {} (epoch: {})', self.best, self.best_epoch)
+
+        self.waits += 1
+        self.logger.info('Patience {} / {}', self.waits, self.patience)
+        if self.waits >= self.patience:
+          return True
+    else:
+      if criterion < self.best - self.eps:
+        self.logger.info('Updating best epoch...')
+        self.logger.info('- Current epoch: {}', criterion)
+        self.logger/info('- Previous best epoch: {} (epoch: {})', self.best, self.best_epoch)
+        self.best_epoch = epoch
+        self.best = criterion
+
+        torch.save(model.state_dicts(), save_dir)
+      else:
+        self.logger.info('Not updating...')
+        self.logger.info('- Current epoch: {}', criterion)
+        self.logger/info('- Previous best epoch: {} (epoch: {})', self.best, self.best_epoch)
+
+        self.waits += 1
+        self.logger.info('Patience {} / {}', self.waits, self.patience)
+        if self.waits >= self.patience:
+          return True
+    
+    return False
