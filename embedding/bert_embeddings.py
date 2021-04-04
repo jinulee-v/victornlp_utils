Added English embeddings and fixed BERT gradient issue

embedding/bert_embeddings.py
@@ -36,8 +36,6 @@ class EmbeddingBERTWordPhr_kor(nn.Module):
     super(EmbeddingBERTWordPhr_kor, self).__init__()
     self.tokenizer = get_tokenizer()
     self.model = BertModel.from_pretrained('monologg/kobert')
-    self.model.eval()
-    self.model.requires_grad = False
     self.embed_size = 1536
     self.special_tokens = config['special_tokens']
 
@@ -64,7 +62,8 @@ class EmbeddingBERTWordPhr_kor(nn.Module):
     sentences = nn.utils.rnn.pad_sequence(sentences, batch_first=True).to(device)
     attention_mask = nn.utils.rnn.pad_sequence(attention_mask, batch_first=True).to(device)
 
-    output = self.model(sentences, attention_mask, torch.zeros_like(attention_mask))['last_hidden_state']
+    with torch.no_grad():
+      output = self.model(sentences, attention_mask, torch.zeros_like(attention_mask))['last_hidden_state']
     embedded = []
     temp = None
     for i, tokens in enumerate(tokens_list):
@@ -172,7 +171,8 @@ class EmbeddingBERTMorph_kor(nn.Module):
       for length, wp_select in zip(lengths, sent_wp_select):
         wp_select.append([length, length])
 
-    output = self.model(sentences, attention_mask, torch.zeros_like(attention_mask))['last_hidden_state']
+    with torch.no_grad():
+      output = self.model(sentences, attention_mask, torch.zeros_like(attention_mask))['last_hidden_state']
 
     
     # return word-phrase embeddings
@@ -200,3 +200,71 @@ class EmbeddingBERTMorph_kor(nn.Module):
         # remove bos token [CLS]
         output = output[:, 1:, :]
       return output
+
+class EmbeddingBERT_eng(nn.Module):
+  """
+  Word-phrase level Embedding model using bert-base-uncased(Google, 2018).
+  Concatenates the hidden state of initial & final wordpiece tokens.
+  """
+  def __init__(self, config):
+    """
+    Constructor for EmbeddingBERTWordPhr_kor.
+    
+    @param self The object pointer.
+    @param config Dictionary. Configuration for EmbeddingBERTWordPhr_kor
+    """
+    super(EmbeddingBERTWordPhr_kor, self).__init__()
+    self.tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+    self.model = BertModel.from_pretrained('bert-base-uncased')
+    self.model.eval()
+    self.model.requires_grad = False
+    self.embed_size = 1536
+    self.special_tokens = config['special_tokens']
+
+  def forward(self, inputs):
+    """
+    Overridden forward().
+    
+    @param self The object pointer.
+    @param inputs List of dictionary. Refer to 'corpus.py' for more details.
+    
+    @return Tensor(batch, length, self.embed_size)
+    """
+    device = next(self.parameters()).device
+    
+    tokens_list = []
+    sentences = []
+    attention_mask = []
+    for input in inputs:
+      tokens = self.tokenizer.tokenize('[CLS] '+input['text']+' [SEP]')
+      tokens_list.append(tokens)
+      tokens = self.tokenizer.convert_tokens_to_ids(tokens)
+      sentences.append(torch.tensor(tokens, dtype=torch.long))
+      attention_mask.append(torch.ones([len(tokens)], dtype=torch.long))
+    sentences = nn.utils.rnn.pad_sequence(sentences, batch_first=True).to(device)
+    attention_mask = nn.utils.rnn.pad_sequence(attention_mask, batch_first=True).to(device)
+
+    with torch.no_grad():
+      output = self.model(sentences, attention_mask, torch.zeros_like(attention_mask))['last_hidden_state']
+    embedded = []
+    temp = None
+    for i, tokens in enumerate(tokens_list):
+      embedded.append([])
+      for j in range(len(tokens)):
+        if tokens[j] == '[SEP]':
+          embedded[i].append(output[i][j].repeat(2).unsqueeze(0))
+          break
+        if tokens[j] == '[CLS]' or not tokens[j].startswith('##'):
+          temp = output[i][j]
+        if j+1 == len(tokens) or tokens[j+1] == '[SEP]' or not tokens[j+1].startswith('##'):
+          temp = torch.cat([temp, output[i][j]], 0)
+          embedded[i].append(temp.unsqueeze(0))
+          temp = None
+      embedded[i] = torch.cat(embedded[i], 0)
+      if 'bos' not in self.special_tokens:
+        embedded[i] = embedded[i][1:]
+      if 'eos' not in self.special_tokens:
+        embedded[i] = embedded[i][:-1]
+    embedded = nn.utils.rnn.pad_sequence(embedded, batch_first=True)
+    return embedded
+