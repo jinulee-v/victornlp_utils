99154724570769fbe592d2cfd6cb369caf09d010
Jinu Lee <aquamrn@snu.ac.kr>
2021-06-29 18:47:26 +0900

    changed pos_tagger to khaiii

--- pos_tagger/korean.py
+++ pos_tagger/korean.py
@@ -1,22 +1,22 @@
 """
 @module pos_tagger/korean
 
-Implements VictorNLP_style wrapper for KOMORAN PoS tagger.
+Implements VictorNLP_style wrapper for Khaiii PoS tagger.
 """
 
-from konlpy.tag import Komoran
+from khaiii import KhaiiiApi
 
 from . import register_pos_tagger
 
 @register_pos_tagger('korean')
-def wrapper_komoran(inputs):
+def wrapper_khaiii(inputs):
   """
-  Korean pos tagger. Uses Komoran by Shineware, wrapped by KoNLPy package.
+  Korean pos tagger. Uses Khaiii from KakaoBrain.
   
   @param inputs List of dictionaries.
   @return inputs Updated inputs.
   """
-  komoran = Komoran()
+  api = KhaiiiApi()
   for input in inputs:
     assert 'text' in input
     assert 'word_count' in input
@@ -29,17 +29,15 @@ def wrapper_komoran(inputs):
     
     pos_tagged = komoran.pos(input['text'].replace(' ', '\n'), flatten=False)
     word_i = 0
-    pos = []
-    for word in pos_tagged:
-      # Beginning of the eojeol
-      pos.append([])
-      
-      for text, pos_tag in word:
-        pos[word_i].append({
-          'text': text,
-          'pos_tag': pos_tag
-        })
-      word_i += 1
+    pos = [
+        [
+          {
+            'id': None,
+            'text': morph.lex,
+            'pos_tag': morph.tag
+          } for morph in word.morphs
+        ] for word in api.analyze(text)
+      ]
     assert len(pos) == input['word_count']
     input['pos'] = pos
 
